82ac88418bd4e9fdb2c9290db690b888a67fc762
Radhika Sharma <sharmaradhika1803@gmail.com>
2026-06-23 18:29:48 +0530

    23/06/2026

--- src/components/MessageCard.tsx
+++ src/components/MessageCard.tsx
@@ -1,3 +1,27 @@
 'use client'
 import react from 'react';
-import {Card, CardContent, CardHeader, CardTitle} from ""
+import {Card, CardContent, CardFooter, CardHeader, CardTitle} from "@/src/components/ui/card";
+import { Button } from './ui/button';
+import { useRouter } from 'next/navigation';
+
+
+const MessageCard = ({message, onDelete}: {message: string, onDelete: () => void}) => {
+  const router = useRouter();
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Message</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <p>{message}</p>
+      </CardContent>
+      <CardFooter>
+        <Button variant="destructive" onClick={onDelete}>
+          Delete
+        </Button>
+      </CardFooter>
+    </Card>
+  );
+};
+
+export default MessageCard;
--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -1,6 +1,6 @@
+import { cn } from "@/src/lib/utils"
 import * as React from "react"
 
-import { cn } from "@/lib/utils"
 
 function Card({
   className,
