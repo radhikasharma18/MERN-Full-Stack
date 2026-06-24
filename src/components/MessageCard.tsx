1185ece56d1838d66ed5962fe41d4d9b713020e6
Radhika Sharma <sharmaradhika1803@gmail.com>
2026-06-24 17:59:15 +0530

    done

--- src/components/MessageCard.tsx
+++ src/components/MessageCard.tsx
@@ -3,20 +3,57 @@ import react from 'react';
 import {Card, CardContent, CardFooter, CardHeader, CardTitle} from "@/src/components/ui/card";
 import { Button } from './ui/button';
 import { useRouter } from 'next/navigation';
+import { AlertDialog, AlertDialogTrigger, AlertDialogContent, AlertDialogHeader, AlertDialogTitle, AlertDialogDescription, AlertDialogFooter } from './ui/alert-dialog';
+import axios from 'axios';
+import { toast } from 'sonner';
 
 
-const MessageCard = ({message, onDelete}: {message: string, onDelete: () => void}) => {
+
+type MessageCardProps = {
+  message: { _id: string };
+  onDelete: (message: { _id: string }) => void;
+};
+const MessageCard = ({message, onDelete}: {message: { _id: string }, onDelete: (message: { _id: string }) => void}) => {
   const router = useRouter();
+  const handleDelete = async() => {
+    const response = await axios.delete<{ Response: string }>(`/api/messages/${message._id}`);       
+    toast.success(response.data.Response);
+    onDelete({ _id: message._id as string });
+    
+  }
   return (
     <Card>
-      <CardHeader>
+      <CardHeader> 
         <CardTitle>Message</CardTitle>
+        <AlertDialog>
+            <AlertDialogTrigger asChild>
+                <Button variant="destructive" className="ml-4">
+                    Delete
+                </Button>
+            </AlertDialogTrigger>
+            <AlertDialogContent>
+                <AlertDialogHeader>
+                    <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+                    <AlertDialogDescription>                    
+                        This action cannot be undone. This will permanently delete your message.
+                    </AlertDialogDescription>
+                </AlertDialogHeader>
+                <AlertDialogFooter>
+                    <Button variant="outline" onClick={() => router.refresh()}>
+                        Cancel
+                    </Button>
+                    <Button variant="destructive" onClick={handleDelete}>
+                        Delete
+                    </Button>
+                </AlertDialogFooter>
+            </AlertDialogContent>
+        </AlertDialog>
       </CardHeader>
       <CardContent>
-        <p>{message}</p>
+        <p>{message._id}</p>
       </CardContent>
       <CardFooter>
-        <Button variant="destructive" onClick={onDelete}>
+       <Button variant="destructive" onClick={handleDelete}>
           Delete
         </Button>
       </CardFooter>
