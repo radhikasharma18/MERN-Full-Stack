6510d1904b8499a981647020fb2c92523ae4f641
Radhika Sharma <sharmaradhika1803@gmail.com>
2026-06-26 18:22:03 +0530

    done

--- src/app/(app)/dashboard/page.tsx
+++ src/app/(app)/dashboard/page.tsx
@@ -1,7 +1,35 @@
 'use client';
 
+import { Message } from "@/src/modules/User"
+import {useCallback, useState } from "react"
+import {toast} from "sonner"
+import { useSession } from "next-auth/react"
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { acceptMessageSchema } from "@/src/schemas/acceptMessageSchema";
+
 const Page = () => {
-  return (
+  const [messages, setMessages] = useState<Message[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [isswitchloaded, setIsSwitchloaded] = useState(false);
+  const [isSwitchOn, setIsSwitchOn] = useState(false);
+  const handleDeleteMessage = (messageId: string) => {
+    setMessages(messages.filter((message) => message._id.toString() !== messageId));
+    toast.success("Message deleted successfully");
+  };
+
+  const { data: session } = useSession();
+  const form = useForm({
+    resolver: zodResolver(acceptMessageSchema),
+    defaultValues: { isAcceptingMessages: false },
+  });
+  const { register, watch, setValue } = form;
+  const acceptMessage = watch("isAcceptingMessages");
+  const fetchAcceptMessage = useCallback (async ()=>{
+    setIsSwitchloaded(false);
+  }, []);
+
+    return (
     <div className="flex justify-center items-center min-h-screen bg-gray-100 px-4">
         <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-xl shadow-md">
             <div className="text-center">
