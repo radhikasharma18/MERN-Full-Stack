9b43cd87c348e3f5e0e5b730bbff306264dc1bcf
radhikasharma18 <sharmaradhika1803@gmail.com>
2026-07-06 22:45:54 +0530

    6/7/2026

---
+++ src/app/(app)/page.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/src/components/ui/carousel";
+
+import { Card, CardContent } from "@/src/components/ui/card";
+import messages from "@/src/messgaes.json";
+import Autoplay from "embla-carousel-autoplay";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <section className="mb-8 text-center md:mb-12">
+        <h1 className="mb-4 text-3xl font-bold">
+          Dive Into the World of Anonymous Messaging: Connect, Share, and
+          Discover with MysteryGuest!
+        </h1>
+
+        <p>
+          Explore Mystery Message — where your identity remains a secret.
+        </p>
+      </section>
+
+      <Carousel
+        className="w-full max-w-4xl"
+        plugins={[Autoplay({ delay: 3000 })]}
+      >
+        <CarouselContent>
+          {messages.map((item, index) => (
+            <CarouselItem key={index}>
+              <Card>
+                <CardContent className="flex min-h-[200px] items-center justify-center p-6">
+                  <p className="text-center text-lg">{item.message}</p>
+                </CardContent>
+              </Card>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
+    </main>
+  );
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import{ Toaster, toast } from "sonner"
 import AuthProvider from "@/src/context/AuthProvider";
-import Navbar from "../components/navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,8 +30,7 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     ><AuthProvider>
       <body className="min-h-full flex flex-col">
-         <Navbar/>
-        {children}
+       {children}
         <Toaster/>
       </body>
     </AuthProvider>
