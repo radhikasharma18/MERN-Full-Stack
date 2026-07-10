94fb771370a64d0099fd52fd0f44d3a0e45c54c1
radhikasharma18 <sharmaradhika1803@gmail.com>
2026-07-10 23:13:30 +0530

    done

--- src/app/u/[username]/page.tsx
+++ src/app/u/[username]/page.tsx
@@ -7,7 +7,7 @@ const Page = ({ params }: { params: { username: string } }) => {
     <div className="flex justify-center items-center min-h-screen bg-gray-100 px-4">
         <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-xl shadow-md">
             <div className="text-center">
-                <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl mb-4">
+                <h1 className="text-4xl tracking-tight lg:text-5xl mb-4">
                     Welcome to Mystery Message
                 </h1>
                 <p className="text-gray-600">
